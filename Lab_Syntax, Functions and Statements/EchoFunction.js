0e9368e4aab5dab169c9c203b2975bdf3532914b
ivanadnin <ivandanin01@gmail.com>
2022-01-12 09:17:41 +0200

    s

---
+++ Lab_Syntax, Functions and Statements/EchoFunction.js
@@ -0,0 +1,6 @@
+let string = readline();
+print();
+function print() {
+    console.log(string.length);
+    console.log(string);
+} 
---
+++ Lab_Syntax, Functions and Statements/StringLength.js
@@ -0,0 +1,9 @@
+
+let a = 'chocolate';
+let b = 'ice cream';
+let c = 'cake';
+function length(a, b, c) {
+    let totalLenght = a.length + b.length + c.length;
+    console.log(totalLenght);
+    console.log(Math.round(totalLenght/3));
+}
