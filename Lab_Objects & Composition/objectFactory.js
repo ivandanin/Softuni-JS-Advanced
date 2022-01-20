ad20263392b5c8e68b5cb4c77fd3c7484bf78a62
ivandanin <ivandanin01@gmail.com>
2022-01-20 16:46:11 +0200

    s

---
+++ Lab_Objects & Composition/objectFactory.js
@@ -0,0 +1,14 @@
+function createProduct(library, orders) {
+    let products = [];
+
+    for (const order of orders) {
+        let product = order.template;
+
+        for (const part of order.parts) {
+            product[part] = library[part];
+
+        }
+        products.push(product);
+    }
+    return products;
+}
