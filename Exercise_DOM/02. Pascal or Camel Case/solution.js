0c3f1299f313bf50af6c5273214aac8d0795af0f
ivanadnin <ivandanin01@gmail.com>
2022-01-24 22:26:04 +0200

    s

--- Exercise_DOM/02. Pascal or Camel Case/solution.js
+++ Exercise_DOM/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,31 @@
 function solve() {
-  //TODO...
+  
+
+  let input = document.getElementById('text').value;
+  let currentCase = document.getElementById('naming-convention').value;
+  let result = '';
+
+  if (currentCase === 'Camel Case') {
+    for (let i = 0; i < input.length; i++) {
+      if (input[i] === ' ') {
+        result += (input[i + 1].toUpperCase());
+        i++;
+      } else {
+          result += (input[i].toLowerCase());
+        }    
+      }
+  } else if (currentCase === 'Pascal Case') {
+    result += input[0].toUpperCase();      
+    for (let i = 1; i < input.length; i++) {
+      if (input[i] === ' ') {
+        result += (input[i + 1].toUpperCase());
+        i++;
+      } else {
+          result += (input[i].toLowerCase());
+        }    
+    }   
+  } else {
+    result = 'Error!';
+  }
+  document.getElementById('result').textContent = result;
 }
