97c7ed1604f031bdf1907117b151729831a7f11b
ivanadnin <ivandanin01@gmail.com>
2022-01-24 16:58:09 +0200

    s

--- Lab_DOM/05.Colorize Table/ColorizeTable.js
+++ Lab_DOM/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,10 @@
 function colorize() {
-    // TODO
+    let rows = document.querySelectorAll('tr');
+    let i = 0;
+    for (const row of rows) {
+        i++;
+        if (i % 2 == 0) {
+            row.style.background = 'teal';
+        }
+    }
 }
