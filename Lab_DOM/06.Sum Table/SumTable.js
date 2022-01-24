d6b9ad90f1b6fae31ee4824aa56b69a952584b46
ivanadnin <ivandanin01@gmail.com>
2022-01-24 17:21:37 +0200

    s

--- Lab_DOM/06.Sum Table/SumTable.js
+++ Lab_DOM/06.Sum Table/SumTable.js
@@ -1,3 +1,10 @@
 function sumTable() {
-
+    let table = document.querySelectorAll('tr');
+    let total = 0;
+    for (let index = 1; index < table.length; index++) {
+        let cols = table[index].children;
+        let cost = cols[cols.length - 1].textContent;
+        total += Number(cost);        
+    }
+    document.getElementById('sum').textContent = total;
 }
