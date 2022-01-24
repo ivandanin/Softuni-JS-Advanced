15b3f78c95d06d4f26dea32aae4105797b6c6ba2
ivanadnin <ivandanin01@gmail.com>
2022-01-24 17:21:56 +0200

    s

--- Lab_DOM/07.Extract Parenthesis/ExtractParenthesis.js
+++ Lab_DOM/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,12 @@
 function extract(content) {
+    let par = document.getElementById(elementId).textContent;
+    let pattern = /\(([^)]+)\)/g;
+    let result = [];
 
+    let match = pattern.exec(par);
+    while (match) {
+        result.push(match[1]);
+        match = pattern.exec(par);
+    }
+    return result.join('; ');
 }
