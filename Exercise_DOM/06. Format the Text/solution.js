39a4ec1de36eebdb6a01f6215bffdb9bfabcd0e1
ivanadnin <ivandanin01@gmail.com>
2022-01-25 11:58:44 +0200

    s

--- Exercise_DOM/06. Format the Text/solution.js
+++ Exercise_DOM/06. Format the Text/solution.js
@@ -1,3 +1,12 @@
 function solve() {
-  //TODO
+  let input = document.getElementById('input');
+  let array = input.value.split('.').filter(s => s !== '');
+  let result = document.getElementById('output');
+
+  while (array.length > 0) {
+    let text = array.splice(0, 3).join('. ') + '.';
+    let p = document.createElement('p');
+    p.textContent = text;
+    result.appendChild(p);
+  }
 }
