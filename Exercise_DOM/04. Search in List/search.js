d6450f7a30339e7d4474dc97dd88c599c7b466f0
ivanadnin <ivandanin01@gmail.com>
2022-01-25 10:15:24 +0200

    s

--- Exercise_DOM/04. Search in List/search.js
+++ Exercise_DOM/04. Search in List/search.js
@@ -1,3 +1,20 @@
 function search() {
-   // TODO
+   let towns = Array.from(document.querySelectorAll('#towns li'));
+   let toSearch = document.getElementById('searchText').value;
+   let result = document.getElementById('result');
+   let counter = 0;
+   
+   for (const town of towns) {
+      if (town.textContent.includes(toSearch) 
+      //edge case
+      && toSearch !== '') {
+         town.style.fontWeight = 'bold';
+         town.style.textDecoration = 'underline';
+         counter++;
+      } else {
+         town.style.fontWeight = 'normal';
+         town.style.textDecoration = 'none';
+      }
+   }
+   result.textContent = `${counter} matches found`
 }
