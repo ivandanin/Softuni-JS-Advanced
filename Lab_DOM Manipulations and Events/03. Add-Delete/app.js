13570e3a556eb3a35f1622876fafc48ebbc5eebb
ivanadnin <ivandanin01@gmail.com>
2022-01-26 12:33:50 +0200

    s

--- Lab_DOM Manipulations and Events/03. Add-Delete/app.js
+++ Lab_DOM Manipulations and Events/03. Add-Delete/app.js
@@ -1,3 +1,28 @@
 function addItem() {
-    //TODO...
-}
+    let addElement = document.getElementById('newItemText').value;
+    let listOfItems = document.getElementById('items');
+
+    if(addElement.length === 0) {
+        return;
+    }
+
+    let newLiElement = document.createElement('li');
+    
+    newLiElement.textContent = addElement;
+    listOfItems.appendChild(newLiElement);
+    document.getElementById('newItemText').value = '';
+
+    let remove = document.createElement('a');
+    let linkText = document.createTextNode('[Delete]');
+    
+    remove.appendChild(linkText);
+    remove.href = '#';
+    remove.addEventListener('click', deleteItem);
+    
+    newLiElement.appendChild(remove);
+    listOfItems.appendChild(newLiElement);
+
+    function deleteItem() {
+        newLiElement.remove();
+    }
+}   
