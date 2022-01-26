67b41b33320c4ac46f1ab98799225bc0c18ec642
ivanadnin <ivandanin01@gmail.com>
2022-01-26 18:24:58 +0200

    s

--- Lab_DOM Manipulations and Events/04. Mouse-Gradient/app.js
+++ Lab_DOM Manipulations and Events/04. Mouse-Gradient/app.js
@@ -1,3 +1,15 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    let gradient = document.getElementById('gradient');
+    gradient.addEventListener('mousemove', gradientMove);
+    gradient.addEventListener('mouseout', gradientOut);
+    
+
+    function gradientMove(event) {
+       let power = Math.floor(event.offsetX / (event.target.clientWidth - 1) * 100);
+       document.getElementById('result').textContent = power + '%';
+    }
+
+    function gradientOut(event) {
+        document.getElementById('result').textContent = '';
+    }
 }
