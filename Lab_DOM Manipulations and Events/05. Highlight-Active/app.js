4dae31c940dfe7c6ce6f13d17551731856880398
ivanadnin <ivandanin01@gmail.com>
2022-01-27 12:27:43 +0200

    s

--- Lab_DOM Manipulations and Events/05. Highlight-Active/app.js
+++ Lab_DOM Manipulations and Events/05. Highlight-Active/app.js
@@ -1,3 +1,21 @@
 function focused() {
-    console.log('TODO:...');
+    let mainDivElement = document.getElementsByTagName('div')[0];
+
+    Array.from(mainDivElement.getElementsByTagName('input')).forEach(e => {
+        e.addEventListener('focus', focus);
+    });
+
+    Array.from(mainDivElement.getElementsByTagName('input')).forEach(e => {
+        e.addEventListener('blur', deFocus);
+    });
+
+    function focus(e) {
+        let parentEl = e.target.parentNode;
+        parentEl.classList.add('focused');
+    }
+
+    function deFocus(e) {
+        let parentEl = e.target.parentNode;
+        parentEl.classList.remove('focused');
+    }
 }
--- Lab_DOM Manipulations and Events/06. Dynamic-Validation/app.js
+++ Lab_DOM Manipulations and Events/06. Dynamic-Validation/app.js
@@ -1,3 +1,8 @@
 function validate() {
-    console.log('TODO:...');
+    let regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    let email = document.getElementById('email').textContent;
+
+    if(!email.match(regex)) {
+        document.getElementById('email').className = '.error';
+    }
 }
