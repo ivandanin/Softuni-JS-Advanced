3498ba82ba47f7ebed3783efb3ceec674c83c00a
ivanadnin <ivandanin01@gmail.com>
2022-01-28 23:14:48 +0200

    s

--- Lab_DOM Manipulations and Events/07. Shopping-Cart/solution.js
+++ Lab_DOM Manipulations and Events/07. Shopping-Cart/solution.js
@@ -1,14 +1,45 @@
 function solve() {
-   let name = document.querySelector('.product');
-   let picture = document.querySelector('.product-image');
-   let price = document.querySelector('.price');
-   let checkout = document.querySelector('textarea');
-   let button = document.querySelector('.add-product');
+  
+   let checkoutTxt = document.querySelector('textarea');
+   let checkoutBtn = document.querySelector('.checkout');
+   let addBtn = Array.from(document.querySelectorAll('.add-product'));
 
-   let textToAppend = `Added ${name} for ${price} to the cart.\n`;
+   addBtn.forEach((btn) => btn.addEventListener('click', addItem));
+   checkoutBtn.addEventListener('click', checkout);
 
-   button.addEventListener('click', function addItem() {
-      checkout.append(textToAppend);
-   });
+   let products = [];
 
+   function addItem(event) {
+      let productContainer = event.target.parentNode.parentNode
+      
+      let title = productContainer.querySelector('.product-title').textContent;
+      let price = Number(productContainer.querySelector('.product-line-price').textContent);
+
+      checkoutTxt.value +=
+       `Added ${title} for ${price.toFixed(2)} to the cart.\n`;
+
+      products.push({name: title, price});
+   }
+
+   function checkout(event) {
+      addBtn.forEach((btn) =>
+       btn.disabled = true);
+
+       let sum = 0;
+       let nameOfProducts = [];
+       
+      for (const product of products) {
+
+         if(!nameOfProducts.includes(product.name)){
+            nameOfProducts.push(product.name);
+         }
+
+         sum += Number(product.price);
+      }
+      
+      checkoutTxt.value +=
+       `You bought ${nameOfProducts.join(', ')} for ${sum.toFixed(2)}.\n`;
+
+      checkoutBtn.disabled = true;
+   }
 }
