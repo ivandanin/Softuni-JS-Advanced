e024b8e9c1ffccd189ff517da161da22947db737
ivanadnin <ivandanin01@gmail.com>
2022-01-29 13:14:23 +0200

    s

--- Exercise_DOM Manipulations and Events/03. Locked-Profile/app.js
+++ Exercise_DOM Manipulations and Events/03. Locked-Profile/app.js
@@ -1,21 +1,21 @@
 function lockedProfile() {
     document.querySelector('#main').addEventListener('click', onClick);
 
-    
     function onClick(event) {
-        const hiddenDiv = event.target.parentElement.querySelector('div');
-        
         let targetEl = event.target;
+        let hiddenDiv = targetEl.parentElement.querySelector('div');
         let btn = targetEl.tagName == 'BUTTON';
         let unlocked = targetEl.parentElement.querySelectorAll('input[type="radio"]')[1];
     
         if (btn && targetEl.textContent == 'Show more') {
+            
             if (unlocked.checked) {
                 hiddenDiv.style.display = 'block';
                 targetEl.textContent = 'Hide it';
-                
+            
             }
         } else if (btn && targetEl.textContent == 'Hide it') {
+
             if (unlocked.checked) {
                 hiddenDiv.style.display = 'none';
                 targetEl.textContent = 'Show more';
--- Exercise_DOM Manipulations and Events/04. Fill-Dropdown/dropdown.js
+++ Exercise_DOM Manipulations and Events/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,15 @@
 function addItem() {
-    console.log('TODO:...');
+    let menu = document.getElementById('menu');
+    let newItemText = document.getElementById('newItemText');
+    let newItemValue = document.getElementById('newItemValue');
+
+    let opt = document.createElement('option');
+    opt.textContent = newItemText.value;
+    opt.value = newItemValue.value;
+
+    menu.appendChild(opt);
+
+    newItemText.value = '';
+    newItemValue.value = '';
+
 }
