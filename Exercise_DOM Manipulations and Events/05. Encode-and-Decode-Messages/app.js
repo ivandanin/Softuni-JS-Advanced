cc23b7f98f1ec91932176d0254e693871b95c262
ivanadnin <ivandanin01@gmail.com>
2022-01-29 15:11:34 +0200

    s

--- Exercise_DOM Manipulations and Events/05. Encode-and-Decode-Messages/app.js
+++ Exercise_DOM Manipulations and Events/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,40 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let [encodeBtn, decodeBtn] = Array.from(
+        document.querySelectorAll('#main div button'));
+
+    encodeBtn.addEventListener('click', encode);
+    decodeBtn.addEventListener('click', decode);
+
+    function encode(event) {
+        let inputTextarea = event.target.parentElement.querySelector('textarea');
+        let encodedMessage = '';
+
+        for (const char of inputTextarea.value) {
+            let currCharCode = char.charCodeAt(0);
+            let newCharCode = currCharCode + 1;
+
+            encodedMessage += String.fromCharCode(newCharCode);
+        }
+
+        let decodedMessage = event.target.parentElement.parentElement.querySelectorAll('textarea')[1];
+
+        decodedMessage.value = encodedMessage;
+        inputTextarea.value = '';
+    }
+
+    function decode(event) {
+        let inputTextarea = event.target.parentElement.querySelector('textarea');
+        let decodedMessage = '';
+
+        for (const char of inputTextarea.value) {
+            let currCharCode = char.charCodeAt(0);
+            let newCharCode = currCharCode - 1;
+
+            decodedMessage += String.fromCharCode(newCharCode);
+        }
+
+        inputTextarea.disabled = false;
+        inputTextarea.value = decodedMessage;
+        inputTextarea.disabled = true;
+    }
 }
