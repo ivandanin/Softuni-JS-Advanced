901ad217c92a30c1c04eae5f068b1ce7b8835772
ivanadnin <ivandanin01@gmail.com>
2022-01-29 12:50:52 +0200

    s

--- Exercise_DOM Manipulations and Events/03. Locked-Profile/app.js
+++ Exercise_DOM Manipulations and Events/03. Locked-Profile/app.js
@@ -1,3 +1,26 @@
 function lockedProfile() {
-    console.log('TODO...')
+    document.querySelector('#main').addEventListener('click', onClick);
+
+    
+    function onClick(event) {
+        const hiddenDiv = event.target.parentElement.querySelector('div');
+        
+        let targetEl = event.target;
+        let btn = targetEl.tagName == 'BUTTON';
+        let unlocked = targetEl.parentElement.querySelectorAll('input[type="radio"]')[1];
+    
+        if (btn && targetEl.textContent == 'Show more') {
+            if (unlocked.checked) {
+                hiddenDiv.style.display = 'block';
+                targetEl.textContent = 'Hide it';
+                
+            }
+        } else if (btn && targetEl.textContent == 'Hide it') {
+            if (unlocked.checked) {
+                hiddenDiv.style.display = 'none';
+                targetEl.textContent = 'Show more';
+                
+            }
+        }
+    }
 }
