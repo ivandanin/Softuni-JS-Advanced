dcb747651a7aa92564267ee5a0fbb6d720d2dc68
ivanadnin <ivandanin01@gmail.com>
2022-02-03 22:25:26 +0200

    s

---
+++ Exercise_Unit Testing and Error Handling/01.RequestValidator/02. Even or Odd/evenOrOdd.test.js
@@ -0,0 +1,20 @@
+let {expect} = require('chai');
+let isOddOrEven = require('./evenOrOdd');
+
+describe('Check if the number is even or odd', () => {
+    it('shouldReturnUndefined', () => {
+        expect(isOddOrEven(1)).to.be.undefined;
+    });
+    
+    it('shouldReturnUndefined', () => {
+        expect(isOddOrEven([1, 2, 3])).to.be.undefined;
+    });
+
+    it('shouldReturnEven', () => {
+        expect(isOddOrEven('13')).to.be.equal('even');
+    }); 
+    
+    it('shouldReturnOdd', () => {
+        expect(isOddOrEven('3')).to.be.equal('odd');
+    }); 
+})
