b4a2ac50972458b95cefc185a9b6629aab44a111
ivanadnin <ivandanin01@gmail.com>
2022-02-07 10:00:31 +0200

    s

--- Exercise_Classes/company.js
+++ Exercise_Classes/company.js
@@ -14,7 +14,7 @@ class Company {
             this.departments[department]
             .push({name, salary, position});
 
-            return `New employee is hired. Name: ${name}. Position: ${position}`;
+            return `New employee is hired. Name: ${name}. Position ${position}`;
         }
     }
 
@@ -45,13 +45,12 @@ class Company {
     }
 }
 
-let c = new Company();
-let actual1 = c.addEmployee("Stanimir", 2000, "engineer", "Human resources");
-c.addEmployee("Pesho", 1500, "electrical engineer", "Construction");
-c.addEmployee("Slavi", 500, "dyer", "Construction");
-c.addEmployee("Stan", 2000, "architect", "Construction");
-c.addEmployee("Stanimir", 1200, "digital marketing manager", "Marketing");
-c.addEmployee("Pesho", 1000, "graphical designer", "Marketing");
-c.addEmployee("Gosho", 1350, "HR", "Human resources");
-
+let c = new Company();
+c.addEmployee("Stanimir", 2000, "engineer", "Construction");
+c.addEmployee("Pesho", 1500, "electrical engineer", "Construction");
+c.addEmployee("Slavi", 500, "dyer", "Construction");
+c.addEmployee("Stan", 2000, "architect", "Construction");
+c.addEmployee("Stanimir", 1200, "digital marketing manager", "Marketing");
+c.addEmployee("Pesho", 1000, "graphical designer", "Marketing");
+c.addEmployee("Gosho", 1350, "HR", "Human resources");
 console.log(c.bestDepartment());
