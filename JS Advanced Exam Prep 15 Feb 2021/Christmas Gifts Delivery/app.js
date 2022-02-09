bad7122bd4e740cb4fc73f42d3b6103b9902d870
ivanadnin <ivandanin01@gmail.com>
2022-02-09 12:58:20 +0200

    s

--- JS Advanced Exam Prep 15 Feb 2021/Christmas Gifts Delivery/app.js
+++ JS Advanced Exam Prep 15 Feb 2021/Christmas Gifts Delivery/app.js
@@ -1,3 +1,57 @@
 function solution() {
-    //TO DO
+    let [gifts, sentGifts, discardGifts] = document.querySelectorAll('section ul')
+    let input = document.querySelector('input');
+
+    document.querySelector('button').addEventListener('click', addGift);
+
+
+    function addGift() {
+        let inputValue = input.value;
+
+        let element = e('li', inputValue, 'gift');
+        let sendBtnEl = e('button', 'Send', 'sendBtn');
+        let discardBtn = e('button', 'Discard', 'discardBtn');
+
+        element.appendChild(sendBtnEl);
+        element.appendChild(discardBtn);
+
+        sendBtnEl.addEventListener('click', () => 
+        sendBtn(inputValue, element));
+
+        discardBtn.addEventListener('click', () => 
+        discardGift(inputValue, element));
+
+        gifts.appendChild(element);
+
+        sortGifts();
+    }
+
+    function sendBtn(name, gift) {
+        gift.remove();
+        let element = e('li', name, 'gift');
+        sentGifts.appendChild(element);
+    }
+
+    function discardGift(name, gift) {
+        gift.remove();
+        let element = e('li', name, 'gift');
+        discardGifts.appendChild(element);
+    }
+
+    function sortGifts() {
+        Array.from(gifts.children)
+        .sort((a, b) => a.textContent.localeCompare(b.textContent))
+        .forEach(g => gifts.appendChild(g));
+    }
+
+    function e(type, content, className) {
+        let result = document.createElement(type);
+        result.textContent = content;
+
+        if (className) {
+            result.className = className;
+        }
+
+        return result;
+    }
 }
