3e50974a945f509800667a067bdfd20b03dd416b
ivanadnin <ivandanin01@gmail.com>
2022-02-12 15:20:20 +0200

    s

--- JS Advanced Exam 23 Oct 2021/02. Camping/camping.js
+++ JS Advanced Exam 23 Oct 2021/02. Camping/camping.js
@@ -12,74 +12,81 @@ class SummerCamp {
 
     registerParticipant(name, condition, money) {
 
-        if (!this.priceForTheCamp[condition]) {
+        if (!Object.keys(this.priceForTheCamp).includes(condition)) {
             throw new Error('Unsuccessful registration at the camp.');
-        }
-    
-        if (this.listOfParticipants.includes(name)) {
-            return `The ${name} is already registered at the camp.`;
-        }
-
-        if (this.priceForTheCamp[condition] > money) {
-            return 'The money is not enough to pay the stay at the camp.';
         } else {
-
-        this.listOfParticipants
-        .push({name,
-             condition,
-              power: 100,
-               wins: 0});
-
-        return `The ${name} was successfully registered.`;
+    
+            if (this.listOfParticipants.map(p => p.name).includes(name)) {
+                return `The ${name} is already registered at the camp.`;
+            }
+        
+            if (this.priceForTheCamp[condition] > money) {
+                return 'The money is not enough to pay the stay at the camp.';
+            } else {
+            
+            this.listOfParticipants
+            .push({name,
+                 condition,
+                  power: 100,
+                   wins: 0});
+            
+            return `The ${name} was successfully registered.`;
+            }
         }
     }
 
     unregisterParticipant(name) {
-        
-        if (!this.listOfParticipants.includes(name)) {
+        let index = this.listOfParticipants.map(p => p.name).indexOf(name);
+        if (index != -1) {
+            this.listOfParticipants.splice(index, 1);
+            return `The ${name} removed successfully.`
+        } else {
             throw new Error(`The ${name} is not registered in the camp.`);
-        } 
-
-        this.listOfParticipants.pop(name);
-        return `The ${name} removed successfully.`
+        }
     }
 
     timeToPlay(typeOfGame, participant1, participant2) {
+        let first = this.listOfParticipants.find(p => p.name == participant1);
+        let second = this.listOfParticipants.find(p => p.name == participant2);
         
-        if (!this.listOfParticipants.includes(participant1) ||
-         !this.listOfParticipants.includes(participant2)) {
-            throw new Error('Invalid entered name/s.');
-        }
-
-        if (participant1[condition] !== participant2[condition]) {
+        switch (typeOfGame) {
+            case 'WaterBalloonFights':
+        if (first && second) {
+        
+        if (first.condition !== second.condition) {
             throw new Error('Choose players with equal condition.');
         }
 
-        switch (typeOfGame) {
-            case 'WaterBalloonFights':
-                if (participant1[power] > participant2[power]) {
-                    participant1[power]++;
-                    return `The ${participant1} is winner in the game ${typeOfGame}.`;
+                if (first.power > second.power) {
+                    first.wins++;
+                    return `The ${first.name} is winner in the game ${typeOfGame}.`;
 
-                } else if (participant2[power] > participant1[power]) {
-                    participant2[power]++;
-                    return `The ${participant2} is winner in the game ${typeOfGame}.`;
+                } else if (second.power > first.power) {
+                    second.wins++;
+                    return `The ${second.name} is winner in the game ${typeOfGame}.`;
                 } else {
                     return 'There is no winner.';
                 }
-
-            case 'Battleship':
-                participant1[power] += 20;
-                return `The ${participant1} successfully completed the game ${typeOfGame}.`;
+        } else {
+            throw new Error('Invalid entered name/s.');
         }
+        case 'Battleship':
+            if (first) {
+                first.power += 20;
+                return `The ${participant1} successfully completed the game ${typeOfGame}.`;
+            } else {
+                throw new Error('Invalid entered name/s.');
+            }
+        }    
     }
 
     toString() {
-        let result = `${organizer} will take ${this.listOfParticipants.length} participants on camping to ${location}`;
+        let result = `${this.organizer} will take ${this.listOfParticipants.length} participants on camping to ${this.location}`;
 
-        this.listOfParticipants.sort((a, b) => a[wins] - b[wins]).forEach(participant => {
-            result += `${participant} - ${participant.condition} - ${participant.power} - ${participant.wins}`;
-        })
+        result += this.listOfParticipants
+                        .sort((a,b) => b.wins - a.wins)
+                        .map(e => `${e.name} - ${e.condition} - ${e.power} - ${e.wins}`)
+                        .join('\n');
 
         return result;
 
@@ -94,7 +101,7 @@ const summerCamp = new SummerCamp("Jane Austen", "Pancharevo Sofia 1137, Bulgari
 
 // console.log(summerCamp.registerParticipant("Petar Petarson", "student", 300));
 // console.log(summerCamp.unregisterParticipant("Petar"));
-// console.log(summerCamp.unregisterParticipant("Petar Petarson"));
+// console.log(summerCamp.unregisterParticipant("Petar Petarson")); // not working properly eventually
 
 // console.log(summerCamp.registerParticipant("Petar Petarson", "student", 300));
 // console.log(summerCamp.timeToPlay("Battleship", "Petar Petarson"));
